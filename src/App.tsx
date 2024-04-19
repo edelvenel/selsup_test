d94ef7b8d8e8891d0fd071b629fdf090f4a2a5f2
Tatiana <edelvenel@yandex.ru>
2024-04-19 20:16:17 +0700

    app.tsx edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,135 @@
 import React from "react";
 import css from "./App.module.scss";
-import { IModel, IParamType } from "./common/types";
-import { ParamEditor } from "./components/ParamEditor";
 import { clone } from "ramda";
-import { STORAGE_KEY_CURRENT, defaultModel, params } from "./common/data";
+
+const STORAGE_KEY_CURRENT = "CURRENT_MODEL";
+
+enum Color {
+  black = "black",
+  white = "white",
+  gray = "gray",
+  red = "red",
+  green = "green",
+  blue = "blue",
+  yellow = "yellow",
+  purple = "purple",
+  pink = "pink",
+}
+
+enum ParamType {
+  string = "string",
+  number = "number",
+  gender = "gender",
+  style = "style",
+}
+
+enum StyleList {
+  classic = "classic",
+  official = "official",
+}
+
+enum GenderList {
+  male = "male",
+  female = "female",
+  unisex = "unisex",
+}
+
+const params: IParam[] = [
+  {
+    id: 1,
+    name: "Назначение",
+    type: ParamType.string,
+  },
+  {
+    id: 2,
+    name: "Длина",
+    type: ParamType.string,
+  },
+];
+
+const defaultModel: IModel = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное",
+    },
+    {
+      paramId: 2,
+      value: "макси",
+    },
+  ],
+  colors: [
+    Color.white,
+    Color.gray,
+    Color.pink,
+    Color.green,
+    Color.blue,
+    Color.purple,
+    Color.yellow,
+  ],
+};
+
+const styles: IStyle[] = [
+  {
+    key: StyleList.classic,
+    value: "Классический",
+  },
+  {
+    key: StyleList.official,
+    value: "Официальный",
+  },
+];
+
+const genders: IGender[] = [
+  {
+    key: GenderList.male,
+    value: "Мужской",
+  },
+  {
+    key: GenderList.female,
+    value: "Женский",
+  },
+  {
+    key: GenderList.unisex,
+    value: "Унисекс",
+  },
+];
+
+interface IParam {
+  id: number;
+  name: string;
+  type: ParamType;
+}
+
+type IString = string;
+type INumber = number;
+
+interface IList {
+  key: IListType;
+}
+
+type IListType = StyleList | GenderList;
+type IParamType = IString | INumber | IList;
+
+interface IParamValue {
+  paramId: number;
+  value: IParamType;
+}
+
+interface IModel {
+  paramValues: IParamValue[];
+  colors: Color[];
+}
+
+interface IStyle {
+  key: StyleList;
+  value: string;
+}
+
+interface IGender {
+  key: GenderList;
+  value: string;
+}
 
 const getData = (): IModel => {
   const currentModel: IModel = JSON.parse(
@@ -67,6 +193,130 @@ function App() {
       </div>
     </div>
   );
+}
+
+interface IParamEditorProps {
+  params: IParam[];
+  model: IModel;
+  onChange: (paramId: number, value: IParamType) => void;
+}
+
+function ParamEditor({ params, model, onChange }: IParamEditorProps) {
+  const getParamData = React.useCallback(
+    (param: IParam) =>
+      model.paramValues.find((paramValue) => param.id === paramValue.paramId),
+    [model]
+  );
+
+  return (
+    <div className={css.paramEditor}>
+      {params.map((param, idx) => (
+        <div key={idx} className={css.param}>
+          <Param
+            paramData={param}
+            paramValue={getParamData(param)}
+            onChange={(value) => onChange(param.id, value)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+interface IParamProps {
+  paramData: IParam;
+  paramValue?: IParamValue;
+  onChange: (value: IParamType) => void;
+}
+
+function Param({ paramData, paramValue, onChange }: IParamProps) {
+  return (
+    <div className={css.paramForm}>
+      <div className={css.paramLabel}>{paramData.name}</div>
+      <div className={css.paramInput}>
+        <ParamInput
+          type={paramData.type}
+          paramValue={paramValue}
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+interface IParamInputProps {
+  type: ParamType;
+  paramValue?: IParamValue;
+  onChange: (value: IParamType) => void;
+}
+
+function ParamInput({ type, paramValue, onChange }: IParamInputProps) {
+  const handleOnChange = (value: IListType) => {
+    const newParam: IParamType = { key: value };
+    onChange(newParam);
+  };
+
+  switch (type) {
+    case ParamType.string: {
+      const value = paramValue?.value as IString;
+      return (
+        <input
+          type="string"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      );
+    }
+
+    case ParamType.number: {
+      const value = paramValue?.value as INumber;
+      return (
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => onChange(Number(e.target.value))}
+        />
+      );
+    }
+
+    case ParamType.gender: {
+      const value = paramValue?.value as IList;
+
+      return (
+        <select
+          id={type}
+          defaultValue={
+            genders.find((gender) => gender.key === value?.key)?.key
+          }
+          onChange={(e) => handleOnChange(e.target.value as IListType)}
+        >
+          {Object.keys(GenderList).map((key, idx) => (
+            <option key={idx} value={key}>
+              {genders.find((gender) => gender.key === key)?.value}
+            </option>
+          ))}
+        </select>
+      );
+    }
+
+    case ParamType.style: {
+      const value = paramValue?.value as IList;
+
+      return (
+        <select
+          id={type}
+          defaultValue={styles.find((style) => style.key === value?.key)?.key}
+          onChange={(e) => handleOnChange(e.target.value as IListType)}
+        >
+          {Object.keys(StyleList).map((key, idx) => (
+            <option key={idx} value={key}>
+              {styles.find((style) => style.key === key)?.value}
+            </option>
+          ))}
+        </select>
+      );
+    }
+  }
 }
 
 export default App;
